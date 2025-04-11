d0d07ca69d6661fbcee3e4511609a5e8d3ae442a
Nguyễn Đình Anh <dinhanh300229@gmail.com>
2025-04-11 15:25:44 +0700

    feat: remove all old image asset

--- components/blog/social-share.tsx
+++ components/blog/social-share.tsx
@@ -73,7 +73,7 @@ export function SocialShare({ postUrl, filePath, title, className }: SocialButto
             <TwitterShareButton
               url={postUrl}
               title={title}
-              via={SITE_METADATA.x}
+              // via={SITE_METADATA.x}
               className="flex items-center gap-2.5 !text-gray-600 hover:!text-gray-900 dark:!text-gray-400 dark:hover:!text-gray-100"
             >
               <XIcon className="h-4 w-4" fill="#fff" viewBox="0 0 1200 1227" />
