25c29ad1d5c51071aa97207672dfa9938f9d9161
Nguyễn Đình Anh <dinhanh300229@gmail.com>
2025-04-30 17:45:58 +0700

    feat: update header link

--- data/navigation.ts
+++ data/navigation.ts
@@ -2,15 +2,28 @@ import { SITE_METADATA } from './site-metadata'
 
 export const HEADER_NAV_LINKS = [
   { href: '/blog', title: 'Blog', emoji: 'writing-hand' },
-  { href: '/snippets', title: 'Snippets', emoji: 'dna' },
-  { href: '/projects', title: 'Projects', emoji: 'man-technologist' },
-  { href: '/about', title: 'About', emoji: 'billed-cap' },
+  // { href: '/snippets', title: 'Snippets', emoji: 'dna' },
+  {
+    href: `${SITE_METADATA.portfolioUrl}?section=projects`,
+    title: 'Projects',
+    emoji: 'man-technologist',
+  },
+  { href: SITE_METADATA.portfolioUrl, title: 'About', emoji: 'billed-cap' },
 ]
 
 export const MORE_NAV_LINKS = [
-  { href: '/books', title: 'Books', emoji: 'books' },
-  { href: '/movies', title: 'Movies', emoji: 'film-frames' },
+  {
+    href: `${SITE_METADATA.portfolioUrl}?section=certifications`,
+    title: 'Certifications',
+    emoji: 'light-bulb',
+  },
+  {
+    href: `${SITE_METADATA.portfolioUrl}?section=achieves`,
+    title: 'Achieves',
+    emoji: 'sports-medal',
+  },
   { href: '/tags', title: 'Tags', emoji: 'label' },
+  { href: `${SITE_METADATA.portfolioUrl}?section=contact`, title: 'Contact', emoji: 'envelope' },
 ]
 
 export const FOOTER_NAV_LINKS = [
