6ad71758b162d01f8cc0e04e18a5fec3d61d7bc7
Nguyễn Đình Anh <dinhanh300229@gmail.com>
2025-07-14 02:25:07 +0700

    feat: fix Unami script

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,7 +129,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			>
 				<TiltedGridBackground className="inset-x-0 top-0 z-[-1] h-[50vh]" />
 				<ThemeProviders>
-					<UmamiAnalytics websiteId={SITE_METADATA.analytics.umamiAnalytics.websiteId} />
+					<UmamiAnalytics
+						websiteId={SITE_METADATA.analytics.umamiAnalytics.websiteId}
+						src="https://cloud.umami.is/script.js"
+					/>
 					<KBarSearchProvider configs={SITE_METADATA.search.kbarConfigs}>
 						<Header />
 						<main className="mb-auto grow">{children}</main>
